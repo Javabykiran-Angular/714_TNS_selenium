bb14170ed17ab6650f9c1d3a8f80f86ddaaf2c5a
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2024-05-09 15:40:55 +0530

    Day07

---
+++ Day07/LocatorsDemo/src/com/tka/locators/LocatorsMain.java
@@ -0,0 +1,19 @@
+package com.tka.locators;
+
+import com.tka.config.Config;
+
+public class LocatorsMain extends Config {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		LocatorsMain obj=new LocatorsMain();
+		
+		obj.browserSetting("file:///D:/SeleniumSoftware/Offline%20Website/index.html");
+		obj.elementAccess();
+		
+		
+
+	}
+
+}
